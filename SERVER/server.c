187acd8a75a092ae6b283cf8145dd2889a8c10d7
yangmuy5 <muyao.yang@mail.utoronto.ca>
2026-03-18 16:37:21 -0400

    Restructured so that fd_set would work in O(1)

--- SERVER/server.c
+++ SERVER/server.c
@@ -4,43 +4,35 @@ int main() {
 
     struct listen_sock passive;
     setup_listen_socket(&passive);
-    
+
+    int max_fd = passive.sock_fd;
+    fd_set all_fds, ready_fds;
+
+    FD_ZERO(&all_fds);
+    FD_SET(passive.sock_fd, &all_fds);
+
     for (;;) {
 
-        fd_set fds;
-        int lfd = passive.sock_fd;
+        ready_fds = all_fds;
 
-        FD_ZERO(&fds);
-        FD_SET(passive.sock_fd, &fds);
-
-        // Build fd_set
-        struct client_sock *curr = get_head();
-        while (curr != NULL) {
-            int curr_fd = curr->sock_fd;
-
-            if (curr_fd > lfd) {
-                lfd = curr_fd;
-            }
-
-            FD_SET(curr_fd, &fds);
-            curr = curr->next;
-        }
-        lfd += 1;
-        
         // Wait until a socket is ready
-        int ret = select(lfd, &fds, NULL, NULL, NULL);
+        int ret = select(max_fd + 1, &ready_fds, NULL, NULL, NULL);
 
         // Accept new connection if ready
-        if (FD_ISSET(passive.sock_fd, &fds)) {
-            // accept
+        if (FD_ISSET(passive.sock_fd, &ready_fds)) {
+            // accept — then add to all_fds:
+            // FD_SET(client_fd, &all_fds);
+            // if (client_fd > max_fd) max_fd = client_fd;
         }
 
         // Read from ready sockets
-        curr = get_head();
+        struct client_sock *curr = get_head();
         while (curr != NULL) {
 
-            if (FD_ISSET(curr->sock_fd), &fds) {
+            if (FD_ISSET(curr->sock_fd, &ready_fds)) {
                 // read stuff
+                // if client disconnects:
+                // FD_CLR(curr->sock_fd, &all_fds);
             }
 
             curr = curr->next;
