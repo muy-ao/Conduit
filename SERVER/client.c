1367eea7f91e4ff82232c1549f2907b6ca21c580
yangmuy5 <yangmuy5>
2026-03-20 00:18:40 +0000

    Added function allowing server to grab message from a given user's buffer

--- SERVER/client.c
+++ SERVER/client.c
@@ -1,71 +1,94 @@
+#include <stdlib.h>
+#include <string.h>
 
 #include "client.h"
-
-struct client_sock {
-    int sock_fd;
-    int inbuf;
-    char buf[BUF_SIZE];
-    struct client_sock *next;
-    struct client_sock *prev;
-};
 
 struct client_sock *head = NULL;
 struct client_sock *tail = NULL;
 
 
 // 1 fail 0 succ
-int add_socket(int sock_fd) {
+int add_client(int sock_fd) {
 
-    struct client_sock *socket = malloc(sizeof(struct client_sock));
-    if (socket == NULL) {
+    struct client_sock *client = malloc(sizeof(struct client_sock));
+    if (client == NULL) {
         return 1;
     }
 
-    socket->next = NULL;
-    socket->prev = NULL;
-    socket->sock_fd = sock_fd;
-    socket->inbuf = 0;
-    memset(socket->buf, 0, BUF_SIZE);
+    client->next = NULL;
+    client->prev = NULL;
+    client->sock_fd = sock_fd;
+    client->inbuf = 0;
+    memset(client->buf, 0, BUF_SIZE);
 
     if (head == NULL) {
-        head = socket;
-        tail = socket;
+        head = client;
+        tail = client;
     } else {
-        socket->prev = tail;
-        tail->next = socket;
-        tail = socket;
+        client->prev = tail;
+        tail->next = client;
+        tail = client;
     }
     return 0;
 }
 
 
-void rm_socket(struct client_sock *socket) {
+void remove_client(struct client_sock *client) {
 
-    if (socket == head && socket == tail) {
+    if (client == head && client == tail) {
         head = NULL;
         tail = NULL;
 
-    } else if (socket == head) {
+    } else if (client == head) {
         head = head->next;
         head->prev = NULL;
 
-    } else if (socket == tail) {
+    } else if (client == tail) {
         tail = tail->prev;
         tail->next = NULL;
 
     } else {
-        socket->prev->next = socket->next;
-        socket->next->prev = socket->prev;
+        client->prev->next = client->next;
+        client->next->prev = client->prev;
     }
 
-    free(socket);
+    free(client);
 }
 
 struct client_sock* get_head() {
     return head;
 } 
 
+int get_message(char **dst, struct client_sock *client) {
 
+    int *inbuf = &(client->inbuf);
+    char *buf = client->buf;
+    
+    int ind = find_CRLF(buf, *inbuf);
+    if (ind == -1) {
+        return 1;
+    }
 
+    int count = ind - 2;
+    char *msg = malloc(count + 1);
+    if (msg == NULL) {
+        return 1;
+    }
 
+    memcpy(msg, buf, count);
+    msg[count] = '\0';
+    *dst = msg;
+    memmove(buf, buf + ind, *inbuf - ind);
+    *inbuf -= ind;
+
+    return 0;
+}
+
+int get_sock_fd(struct client_sock *client) {
+    return client->sock_fd;
+}
+
+struct client_sock* get_next(struct client_sock *client) {
+    return client->next;
+}
 
--- SERVER/client.h
+++ SERVER/client.h
@@ -1,10 +1,21 @@
 #ifndef _CLIENT_H_
 #define _CLIENT_H_
 
-#define BUF_SIZE 1024
+#include "socket.h"
 
-int add_socket(int sock_fd);
-void rm_socket(struct client_sock *socket);
+struct client_sock {
+    int sock_fd;
+    int inbuf;
+    char buf[BUF_SIZE];
+    struct client_sock *next;
+    struct client_sock *prev;
+};
+
+int add_client(int sock_fd);
+void remove_client(struct client_sock *socket);
 struct client_sock* get_head(void);
+int get_message(char **dst, struct client_sock *client);
+int get_sock_fd(struct client_sock *client);
+struct client_sock* get_next(struct client_sock *client);
 
 #endif
